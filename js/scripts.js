462ef7f12a03e2a54555e936d623d0b2498f32e3
sexfat <bryant6831@gmail.com>
2021-06-22 09:55:10 +0800

    add jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
-import $ from 'jquery';
+import $ from 'jquery'; // jquery 
+import TweenMax from 'gsap'; // gsap
 
-$('body').css('background-color' , 'red');
+$('body').css('background-color' , 'green');
+
+
+TweenMax.to('.box' , 1,{
+   x: 400,
+   rotation : 180    
+} )
 
 
 let age = 26;
