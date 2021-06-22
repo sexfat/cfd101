5de714bca49e01faa0b506e89bfacbb513f553aa
sexfat <bryant6831@gmail.com>
2021-06-22 10:27:22 +0800

    add webpack config

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,19 @@ import TweenMax from 'gsap'; // gsap
 $('body').css('background-color' , 'green');
 
 
-TweenMax.to('.box' , 1,{
+TweenMax.to('.box' , 2,{
    x: 400,
-   rotation : 180    
+   rotation : 180,
+   backgroundColor : 'white' ,
+   borderRadius: '50%'   
 } )
 
 
 let age = 26;
+
+// gulp
+//src  進入點
+// dest 輸出點 
 
 
 
